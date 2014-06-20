7b47ea45511607aaa2c606cbe3e6d797cd502eff
Martin Treusch von Buttlar <martin@t17r.de>
2014-06-20 22:49:02 +0200

    rename instance name to store

--- mgostore/mgostorage.go
+++ mgostore/mgostorage.go
@@ -38,84 +38,84 @@ func New(session *mgo.Session, dbName string) *MongoStorage {
 	return storage
 }
 
-func (oauth *MongoStorage) GetClient(id string) (*osin.Client, error) {
-	session := oauth.session.Copy()
+func (store *MongoStorage) GetClient(id string) (*osin.Client, error) {
+	session := store.session.Copy()
 	defer session.Close()
-	clients := session.DB(oauth.dbName).C(CLIENT_COL)
+	clients := session.DB(store.dbName).C(CLIENT_COL)
 	client := new(osin.Client)
 	err := clients.FindId(id).One(client)
 	return client, err
 }
 
-func (oauth *MongoStorage) SetClient(id string, client *osin.Client) error {
-	session := oauth.session.Copy()
+func (store *MongoStorage) SetClient(id string, client *osin.Client) error {
+	session := store.session.Copy()
 	defer session.Close()
-	clients := session.DB(oauth.dbName).C(CLIENT_COL)
+	clients := session.DB(store.dbName).C(CLIENT_COL)
 	_, err := clients.UpsertId(id, client)
 	return err
 }
 
-func (oauth *MongoStorage) SaveAuthorize(data *osin.AuthorizeData) error {
-	session := oauth.session.Copy()
+func (store *MongoStorage) SaveAuthorize(data *osin.AuthorizeData) error {
+	session := store.session.Copy()
 	defer session.Close()
-	authorizations := session.DB(oauth.dbName).C(AUTHORIZE_COL)
+	authorizations := session.DB(store.dbName).C(AUTHORIZE_COL)
 	_, err := authorizations.UpsertId(data.Code, data)
 	return err
 }
 
-func (oauth *MongoStorage) LoadAuthorize(code string) (*osin.AuthorizeData, error) {
-	session := oauth.session.Copy()
+func (store *MongoStorage) LoadAuthorize(code string) (*osin.AuthorizeData, error) {
+	session := store.session.Copy()
 	defer session.Close()
-	authorizations := session.DB(oauth.dbName).C(AUTHORIZE_COL)
+	authorizations := session.DB(store.dbName).C(AUTHORIZE_COL)
 	authData := new(osin.AuthorizeData)
 	err := authorizations.FindId(code).One(authData)
 	return authData, err
 }
 
-func (oauth *MongoStorage) RemoveAuthorize(code string) error {
-	session := oauth.session.Copy()
+func (store *MongoStorage) RemoveAuthorize(code string) error {
+	session := store.session.Copy()
 	defer session.Close()
-	authorizations := session.DB(oauth.dbName).C(AUTHORIZE_COL)
+	authorizations := session.DB(store.dbName).C(AUTHORIZE_COL)
 	return authorizations.RemoveId(code)
 }
 
-func (oauth *MongoStorage) SaveAccess(data *osin.AccessData) error {
-	session := oauth.session.Copy()
+func (store *MongoStorage) SaveAccess(data *osin.AccessData) error {
+	session := store.session.Copy()
 	defer session.Close()
-	accesses := session.DB(oauth.dbName).C(ACCESS_COL)
+	accesses := session.DB(store.dbName).C(ACCESS_COL)
 	_, err := accesses.UpsertId(data.AccessToken, data)
 	return err
 }
 
-func (oauth *MongoStorage) LoadAccess(token string) (*osin.AccessData, error) {
-	session := oauth.session.Copy()
+func (store *MongoStorage) LoadAccess(token string) (*osin.AccessData, error) {
+	session := store.session.Copy()
 	defer session.Close()
-	accesses := session.DB(oauth.dbName).C(ACCESS_COL)
+	accesses := session.DB(store.dbName).C(ACCESS_COL)
 	accData := new(osin.AccessData)
 	err := accesses.FindId(token).One(accData)
 	return accData, err
 }
 
-func (oauth *MongoStorage) RemoveAccess(token string) error {
-	session := oauth.session.Copy()
+func (store *MongoStorage) RemoveAccess(token string) error {
+	session := store.session.Copy()
 	defer session.Close()
-	accesses := session.DB(oauth.dbName).C(ACCESS_COL)
+	accesses := session.DB(store.dbName).C(ACCESS_COL)
 	return accesses.RemoveId(token)
 }
 
-func (oauth *MongoStorage) LoadRefresh(token string) (*osin.AccessData, error) {
-	session := oauth.session.Copy()
+func (store *MongoStorage) LoadRefresh(token string) (*osin.AccessData, error) {
+	session := store.session.Copy()
 	defer session.Close()
-	accesses := session.DB(oauth.dbName).C(ACCESS_COL)
+	accesses := session.DB(store.dbName).C(ACCESS_COL)
 	accData := new(osin.AccessData)
 	err := accesses.Find(bson.M{REFRESHTOKEN: token}).One(accData)
 	return accData, err
 }
 
-func (oauth *MongoStorage) RemoveRefresh(token string) error {
-	session := oauth.session.Copy()
+func (store *MongoStorage) RemoveRefresh(token string) error {
+	session := store.session.Copy()
 	defer session.Close()
-	accesses := session.DB(oauth.dbName).C(ACCESS_COL)
+	accesses := session.DB(store.dbName).C(ACCESS_COL)
 	return accesses.Update(bson.M{REFRESHTOKEN: token}, bson.M{
 		"$unset": bson.M{
 			REFRESHTOKEN: 1,
